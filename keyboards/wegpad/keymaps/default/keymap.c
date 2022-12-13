b0a5ffa6bbee8691ddd865755a39d59aade8fadb
Shawn <shawn@chaoticweg.cc>
2022-12-12 22:56:12 -0600

    modify keymap to match actual layout

--- keyboards/wegpad/keymaps/default/keymap.c
+++ keyboards/wegpad/keymaps/default/keymap.c
@@ -5,20 +5,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┬───┬───┬───┬───┐
      * │ A │ B │ C │ D │ E │
      * ├───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │
+     * │ F │ G │ H │ I │ J │
      * ├───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │
+     * │ K │ L │ M │   │ > │ <-- [2, 4] is encoder click
      * ├───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │
+     * │ P │ Q │ R │   │   │
      * ├───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │
+     * │ U │ V │ W │   │   │
      * └───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_5x5(
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E
+        KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_K,    KC_L,    KC_M,    KC_NO,   KC_MPLY,
+        KC_P,    KC_Q,    KC_R,    KC_NO,   KC_NO,
+        KC_U,    KC_V,    KC_W,    KC_NO,   KC_NO
     )
 };
